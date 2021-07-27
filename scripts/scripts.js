31979f66bced3f15c8f0064113a39f733903254e
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2021-07-27 12:34:03 -0300

    [REQUIREMENT] proceeds only if user chooses (even >=4 <=14) number of cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
-let numberOfCards = 0;
+let numberOfCards;
 do {
-    numberOfCards = prompt("Com quantas cartas você quer jogar?");
-} while ((numberOfCards%2 === 0) && (numberOfCards >= 4) && (numberOfCards <= 14)) 
-console.log(numberOfCards);
+    numberOfCards = prompt("Com quantas cartas você deseja jogar?");
+    console.log(numberOfCards + " repetir qtde cartas");
+ } while (((numberOfCards % 2) !== 0) || (numberOfCards < 4) || (numberOfCards > 14)); 
+console.log(numberOfCards + " continuar");
