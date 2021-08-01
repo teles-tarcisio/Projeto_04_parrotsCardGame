7840f148e55e54f8365e061d87532c3ade10b5dc
Tarcísio Teles <tfst@poli.br>
2021-08-01 16:28:09 -0300

    [REQUIREMENT] cards distributed randomly according to number desired by user, working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,14 @@
-let numberOfCards = 0;
-askQuantity();
+const numberOfCards = askQuantity();
 function askQuantity() {
+    let howManyCards = 0;
     do {
-        numberOfCards = parseInt(prompt("Com quantas cartas você deseja jogar?"));
-        console.log(typeof(numberOfCards));
-        console.log(numberOfCards + ", fornecido");
-     } while ((numberOfCards % 2 !== 0) || !((numberOfCards >= 4) && (numberOfCards <= 14)));
-    console.log(numberOfCards + " continuar");
+        howManyCards = parseInt(prompt("Com quantas cartas você deseja jogar?"));
+        console.log(typeof (howManyCards));
+        console.log(howManyCards + ", fornecido");
+    } while ((howManyCards % 2 !== 0) || !((howManyCards >= 4) && (howManyCards <= 14)));
+    console.log(howManyCards + " continuar");
 
+    return howManyCards;
 }
 
 
@@ -15,26 +16,52 @@ function askQuantity() {
 //  array element contains gifs and "content" of the card;
 //      div contains ONE front img and ONE array element!
 
-const gameCards = [];
-let shuffledCards = [];
+
+function initializeAllCards() {
+    let allCards = [];
+    allCards.length = 7;
+    allCards[0] = "images/bobrossparrot.gif";
+    allCards[1] = "images/explodyparrot.gif";
+    allCards[2] = "images/fiestaparrot.gif";
+    allCards[3] = "images/metalparrot.gif";
+    allCards[4] = "images/revertitparrot.gif";
+    allCards[5] = "images/tripletsparrot.gif";
+    allCards[6] = "images/unicornparrot.gif";
+    console.log("allCards, original: " + allCards.toString());
+
+    //shuffle full array of cards:
+    allCards = allCards.sort(randomComparator);
+    console.log("allCards, shuffled: " + allCards.toString());
+
+    return allCards;
+}
 function randomComparator() {
     return Math.random() - 0.5;
 }
-function randomizeArrayofCards(quantity) {
-    gameCards[0] = "images/bobrossparrot.gif";
-    gameCards[1] = "images/explodyparrot.gif";
-    gameCards[2] = "images/fiestaparrot.gif";
-    gameCards[3] = "images/metalparrot.gif";
-    gameCards[4] = "images/revertitparrot.gif";
-    gameCards[5] = "images/tripletsparrot.gif";
-    gameCards[6] = "images/unicornparrot.gif";
-    console.log("before: " + gameCards.toString());
-    
-    //shuffling:
-    shuffledCards = gameCards.sort(randomComparator);
-    console.log("after: " + shuffledCards.toString());
+const fixedCards = initializeAllCards();
+
+function distributeCards() {
+    let temporaryCards = [];
+    temporaryCards.length = numberOfCards;
+    console.log(fixedCards.toString());
+    console.log(temporaryCards.toString());
+
+    for (let i = 0; i < numberOfCards/2; i++) {
+        temporaryCards[i] = fixedCards[i];
+    }
+    console.log(temporaryCards.toString());
+
+    for (let j = numberOfCards/2; j < numberOfCards; j++) {
+        temporaryCards[j] = temporaryCards[j % (numberOfCards/2)];
+    }
+    console.log(temporaryCards.toString());
+
+    temporaryCards = temporaryCards.sort(randomComparator);
+
+    return temporaryCards;
 }
-randomizeArrayofCards();
+const gameCards = distributeCards();
+
 
 //# of divs == # of cards: (the div contains the array element)
 createCardSlots(numberOfCards);
@@ -45,18 +72,18 @@ function createCardSlots(quantity) {    //creates # of divs according to user in
         //create <div>:
         newDivCard = document.createElement("div");
         newDivCard.className = "card-slot";
-        
+
         //newDivCard.setAttribute( ... )
 
         //create and add front <img> (parrot):
         newImg = document.createElement("img");
-        newImg.setAttribute("src", "images/front_deck.png");
+        //newImg.setAttribute("src", "images/front_deck.png");
         newImg.className = "default-deck-image";
         newDivCard.appendChild(newImg);
 
         //create and add back <img> (gif):
         newImg = document.createElement("img");
-        newImg.setAttribute("src", "images/bobrossparrot.gif");
+        newImg.setAttribute("src", gameCards[counter]);
         newImg.className = "bob";
         newDivCard.appendChild(newImg);
 
